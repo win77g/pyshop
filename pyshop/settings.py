739501619fb5086761e1fce8e48c1b930a3c3ca7
win21g <win21g@mail.ru>
2019-01-05 19:35:17 +0300

    активация почты

--- pyshop/settings.py
+++ pyshop/settings.py
@@ -145,8 +145,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  465
 EMAIL_HOST_USER = 'sergsergio777@gmail.com'
 EMAIL_HOST_PASSWORD = 'marmon30'
-EMAIL_HOST_TLS = False
-EMAIL_HOST_SSL = True
+# EMAIL_HOST_TLS = False
+# EMAIL_HOST_SSL = True
 
 # Настройки ckeitor
 CKEDITOR_UPLOAD_PATH = "uploads/"
